5e73d5d173208cdda74da0f21dce639da77ce07a
Shivam Mishra <shivam.mishra@wakencode.com>
2021-11-24 04:10:28 -0800

    Website: Shivam Mishra

--- Readme.txt
+++ Readme.txt
@@ -1,6 +1,1 @@
-Thanks for downloading this template!
-
-Template Name: iPortfolio
-Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
-Author: BootstrapMade.com
-License: https://bootstrapmade.com/license/
+Hey!! I'm shivam mishra and you are welcome
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,5 @@
-/**
-* Template Name: iPortfolio - v3.3.0
-* Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-(function() {
-  "use strict";
+;(function () {
+  'use strict'
 
   /**
    * Easy selector helper function
@@ -26,7 +20,7 @@
     let selectEl = select(el, all)
     if (selectEl) {
       if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
+        selectEl.forEach((e) => e.addEventListener(type, listener))
       } else {
         selectEl.addEventListener(type, listener)
       }
@@ -34,7 +28,7 @@
   }
 
   /**
-   * Easy on scroll event listener 
+   * Easy on scroll event listener
    */
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
@@ -46,11 +40,14 @@
   let navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
     let position = window.scrollY + 200
-    navbarlinks.forEach(navbarlink => {
+    navbarlinks.forEach((navbarlink) => {
       if (!navbarlink.hash) return
       let section = select(navbarlink.hash)
       if (!section) return
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+      if (
+        position >= section.offsetTop &&
+        position <= section.offsetTop + section.offsetHeight
+      ) {
         navbarlink.classList.add('active')
       } else {
         navbarlink.classList.remove('active')
@@ -67,7 +64,7 @@
     let elementPos = select(el).offsetTop
     window.scrollTo({
       top: elementPos,
-      behavior: 'smooth'
+      behavior: 'smooth',
     })
   }
 
@@ -90,7 +87,7 @@
   /**
    * Mobile nav toggle
    */
-  on('click', '.mobile-nav-toggle', function(e) {
+  on('click', '.mobile-nav-toggle', function (e) {
     select('body').classList.toggle('mobile-nav-active')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
@@ -99,20 +96,25 @@
   /**
    * Scrool with ofset on links with a class name .scrollto
    */
-  on('click', '.scrollto', function(e) {
-    if (select(this.hash)) {
-      e.preventDefault()
+  on(
+    'click',
+    '.scrollto',
+    function (e) {
+      if (select(this.hash)) {
+        e.preventDefault()
 
-      let body = select('body')
-      if (body.classList.contains('mobile-nav-active')) {
-        body.classList.remove('mobile-nav-active')
-        let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bi-list')
-        navbarToggle.classList.toggle('bi-x')
+        let body = select('body')
+        if (body.classList.contains('mobile-nav-active')) {
+          body.classList.remove('mobile-nav-active')
+          let navbarToggle = select('.mobile-nav-toggle')
+          navbarToggle.classList.toggle('bi-list')
+          navbarToggle.classList.toggle('bi-x')
+        }
+        scrollto(this.hash)
       }
-      scrollto(this.hash)
-    }
-  }, true)
+    },
+    true
+  )
 
   /**
    * Scroll with ofset on page load with hash links in the url
@@ -123,7 +125,7 @@
         scrollto(window.location.hash)
       }
     }
-  });
+  })
 
   /**
    * Hero type effect
@@ -137,24 +139,24 @@
       loop: true,
       typeSpeed: 100,
       backSpeed: 50,
-      backDelay: 2000
-    });
+      backDelay: 2000,
+    })
   }
 
   /**
    * Skills animation
    */
-  let skilsContent = select('.skills-content');
+  let skilsContent = select('.skills-content')
   if (skilsContent) {
     new Waypoint({
       element: skilsContent,
       offset: '80%',
-      handler: function(direction) {
-        let progress = select('.progress .progress-bar', true);
+      handler: function (direction) {
+        let progress = select('.progress .progress-bar', true)
         progress.forEach((el) => {
           el.style.width = el.getAttribute('aria-valuenow') + '%'
-        });
-      }
+        })
+      },
     })
   }
 
@@ -162,38 +164,42 @@
    * Porfolio isotope and filter
    */
   window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
+    let portfolioContainer = select('.portfolio-container')
     if (portfolioContainer) {
       let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item'
-      });
+        itemSelector: '.portfolio-item',
+      })
 
-      let portfolioFilters = select('#portfolio-flters li', true);
+      let portfolioFilters = select('#portfolio-flters li', true)
 
-      on('click', '#portfolio-flters li', function(e) {
-        e.preventDefault();
-        portfolioFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
-        });
-        this.classList.add('filter-active');
+      on(
+        'click',
+        '#portfolio-flters li',
+        function (e) {
+          e.preventDefault()
+          portfolioFilters.forEach(function (el) {
+            el.classList.remove('filter-active')
+          })
+          this.classList.add('filter-active')
 
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-        portfolioIsotope.on('arrangeComplete', function() {
-          AOS.refresh()
-        });
-      }, true);
+          portfolioIsotope.arrange({
+            filter: this.getAttribute('data-filter'),
+          })
+          portfolioIsotope.on('arrangeComplete', function () {
+            AOS.refresh()
+          })
+        },
+        true
+      )
     }
-
-  });
+  })
 
   /**
-   * Initiate portfolio lightbox 
+   * Initiate portfolio lightbox
    */
   const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
+    selector: '.portfolio-lightbox',
+  })
 
   /**
    * Portfolio details slider
@@ -203,14 +209,14 @@
     loop: true,
     autoplay: {
       delay: 5000,
-      disableOnInteraction: false
+      disableOnInteraction: false,
     },
     pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
-      clickable: true
-    }
-  });
+      clickable: true,
+    },
+  })
 
   /**
    * Testimonials slider
@@ -220,26 +226,26 @@
     loop: true,
     autoplay: {
       delay: 5000,
-      disableOnInteraction: false
+      disableOnInteraction: false,
     },
     slidesPerView: 'auto',
     pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
-      clickable: true
+      clickable: true,
     },
     breakpoints: {
       320: {
         slidesPerView: 1,
-        spaceBetween: 20
+        spaceBetween: 20,
       },
 
       1200: {
         slidesPerView: 3,
-        spaceBetween: 20
-      }
-    }
-  });
+        spaceBetween: 20,
+      },
+    },
+  })
 
   /**
    * Animation on scroll
@@ -249,8 +255,7 @@
       duration: 1000,
       easing: 'ease-in-out',
       once: true,
-      mirror: false
+      mirror: false,
     })
-  });
-
+  })
 })()
